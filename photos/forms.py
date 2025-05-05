194693d853a4c27ce211514c0e1fb388ac532ab9
Luthor Garret <luthorga@student.21-school.ru>
2025-05-06 00:46:52 +0700

    small fix

--- photos/forms.py
+++ photos/forms.py
@@ -1,65 +1,51 @@
 from django import forms
-from .models import Photo, Category, Comment
-
+from .models import Photo, Category, PhotoCategory, Tag, Comment
 
 class PhotoForm(forms.ModelForm):
-    tags = forms.CharField(
-        required=False,
-        widget=forms.TextInput(
-            attrs={'placeholder': 'Введите теги через запятую'}),
-        help_text='Введите теги через запятую, например: природа, горы, закат')
-
+    tags = forms.CharField(required=False, 
+                          help_text="Введите теги через запятую")
+    
     class Meta:
         model = Photo
-        fields = ['title', 'image', 'description', 'category', 'category_type']
-
+        fields = ['title', 'image', 'description', 'category', 'category_type', 'tags']
+        
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # If we're editing an existing photo, populate the tags field
         if self.instance.pk:
-            self.initial['tags'] = ', '.join(
-                [tag.name for tag in self.instance.tags.all()])
-
+            # If editing an existing photo, populate the tags field
+            self.initial['tags'] = ', '.join([tag.name for tag in self.instance.tags.all()])
+    
     def save(self, commit=True):
-        instance = super().save(commit=False)
-
+        photo = super().save(commit=False)
+        
         if commit:
-            instance.save()
-
-        # Handle tags with taggit
-        if 'tags' in self.cleaned_data:
-            # Clear existing tags
-            instance.tags.clear()
-
-            # Add new tags
-            tag_string = self.cleaned_data['tags']
-            if tag_string:
-                tag_list = [
-                    tag.strip() for tag in tag_string.split(',')
-                    if tag.strip()
-                ]
-                instance.tags.add(*tag_list)
-
-        return instance
-
+            photo.save()
+            
+            # Handle tags
+            if 'tags' in self.cleaned_data and self.cleaned_data['tags']:
+                # Clear existing tags
+                photo.tags.clear()
+                
+                # Process and add new tags
+                tag_names = [name.strip() for name in self.cleaned_data['tags'].split(',') if name.strip()]
+                for tag_name in tag_names:
+                    tag, created = Tag.objects.get_or_create(
+                        name=tag_name,
+                        defaults={'slug': slugify(tag_name)}
+                    )
+                    photo.tags.add(tag)
+                    
+        return photo
 
 class CategoryForm(forms.ModelForm):
-
     class Meta:
         model = Category
         fields = ['name', 'description']
 
-
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ['text']
         widgets = {
-            'text':
-            forms.Textarea(attrs={
-                'rows': 4,
-                'placeholder': 'Введите ваш комментарий'
-            })
+            'text': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Оставьте комментарий...'})
         }
